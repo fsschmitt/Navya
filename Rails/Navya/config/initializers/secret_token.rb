954c76a093551cc0288d3b13c5d140fb2b0fb07d
Felipe Schmitt <fsschmitt@gmail.com>
2012-05-02 19:46:42 +0100

    Rails

---
+++ Rails/Navya/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Navya::Application.config.secret_token = '2d92bca4b9a5518d2fa802dc6486bac1a005a90c43d09a66cbce101b9140cd646ddfea008834d5c1f4b9d55c287b034852d279232cb986b79dd9474e445bd73d'
