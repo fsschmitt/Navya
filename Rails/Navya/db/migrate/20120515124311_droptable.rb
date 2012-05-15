bfe27547ac78ca2b336bfc5724f53701d71eaa38
Felipe Schmitt <fsschmitt@gmail.com>
2012-05-15 15:09:56 +0100

    full page background

--- Rails/Navya/db/migrate/20120515124311_droptable.rb
+++
@@ -1,8 +0,0 @@
-class Droptable < ActiveRecord::Migration
-  def up
-  	drop_table :noticia
-  end
-
-  def down
-  end
-end
--- Rails/Navya/db/schema.rb
+++ Rails/Navya/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120515124311) do
+ActiveRecord::Schema.define(:version => 20120515124039) do
 
   create_table "news", :force => true do |t|
     t.string   "url"
